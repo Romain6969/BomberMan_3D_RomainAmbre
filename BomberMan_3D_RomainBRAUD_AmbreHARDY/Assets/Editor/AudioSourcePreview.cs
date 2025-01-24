1b07ba6bca92bf3ac3b7879155121c15d1ce833a
MeguminFireFox <ambrehardy013@gmail.com>
2025-01-24 13:23:18 +0100

    Amélioration de L'IA et Fin

--- BomberMan_3D_RomainBRAUD_AmbreHARDY/Assets/Editor/AudioSourcePreview.cs
+++ BomberMan_3D_RomainBRAUD_AmbreHARDY/Assets/Editor/AudioSourcePreview.cs
@@ -18,7 +18,7 @@ public class AudioSourcePreview : Editor
             }
             else
             {
-                Debug.LogWarning("Aucun clip audio assigné !");
+                Debug.LogWarning("Aucun clip");
             }
         }
     }
